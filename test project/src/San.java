ddbc4e4de1ccb6df69c1c11610e7ca43a57a35f5
Ganeshnagappan19 <104415486+Ganeshnagappan19@users.noreply.github.com>
2022-04-27 17:38:51 +0530

    Update San.java

--- test project/src/San.java
+++ test project/src/San.java
@@ -4,6 +4,7 @@ public class San {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("hhhhhh");
+		system.out.println("ganesh agaram");
 	}
 
 }
